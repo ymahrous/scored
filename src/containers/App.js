044a943ecc491741f146b0bbf3c8a57e04ea50cd
Yousef Mahrous <yousefzentral@gmail.com>
2023-08-05 15:25:11 +0300

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import About from '../components/About/About';
 import Getscore from '../components/Home/Home';
-import Navigation from '../components/Nav/Nav';
 import Footer from '../components/Footer/Footer';
-import Contact from '../components/Contact/Contact';
 import { BrowserRouter as Router, Route , Switch} from 'react-router-dom';
 export default function App() {
   return (
@@ -12,16 +9,6 @@ export default function App() {
       <Switch>
         <Route exact path='/'>
           <Getscore />
-        </Route>
-      </Switch>
-      <Switch>
-        <Route exact path='/about'>
-          <About />
-        </Route>
-      </Switch>
-      <Switch>
-        <Route exact path='/contact'>
-          <Contact />
         </Route>
       </Switch>
       <Footer />
